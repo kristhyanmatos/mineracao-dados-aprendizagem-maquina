25794c80b7d97d5720b602ceb703a3f81a58c384
kristhyanmatos <kristhyandematosmaia@gmail.com>
2021-08-08 13:28:13 -0300

    feat: Seleciona colunas úteis e Remove ruídos

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# mineracao-dados-aprendizagem-maquina
+# Mineração de dados e aprendizagem máquina
+
+# Normatização dos dados
+
+    - STATUS_ANALISE -
+
+        * Não Concluída -> 0
+        * Concluída -> 1
+
+    - PRODUTO_MOTIVO -
+
+        Uso de sangue ou componente -> 0
+
+    - TIPO_REACAO_TRANSFUSIONAL -
---
+++ src/main.py
@@ -0,0 +1,42 @@
+import ssl
+import pandas
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Fonte do dado
+# https://dados.anvisa.gov.br/dados/DADOS_ABERTOS_HEMOVIGILANCIA.csv
+
+dados_brutos = pandas.read_csv(
+    "DADOS_ABERTOS_HEMOVIGILANCIA.csv",
+    encoding="ISO-8859-1",
+    sep=";",
+)
+
+"""Pré-processamento"""
+
+# Seleciona colunas úteis
+dados_brutos = dados_brutos[
+    [
+        "STATUS_ANALISE",
+        "PRODUTO_MOTIVO",
+        "TIPO_REACAO_TRANSFUSIONAL",
+        "GRAU_RISCO",
+        "TIPO_HEMOCOMPONENTE",
+        "FAIXA_ETARIA_PACIENTE",
+        "DS_TEMPORALIDADE_REACAO",
+        "TIPO_EVENTO_ADVERSO",
+    ]
+]
+
+# Remove ruídos
+dados_brutos = dados_brutos[
+    (dados_brutos["GRAU_RISCO"] != "Não informado")
+    & (dados_brutos["TIPO_HEMOCOMPONENTE"] != "NÃO INFORMADO")
+    & (dados_brutos["TIPO_REACAO_TRANSFUSIONAL"].notnull())
+]
+
+# Normatização dos dados
+
+
+dados_brutos.to_csv("dados.csv")
+print(dados_brutos)
