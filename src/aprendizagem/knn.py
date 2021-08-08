8906c5a0833c8963d6ef215a9d9b6bb593a24800
kristhyanmatos <kristhyandematosmaia@gmail.com>
2021-08-08 18:31:29 -0300

    feat: arquitetura

---
+++ src/aprendizagem/knn.py
@@ -0,0 +1,35 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from pre_processamento_dados import PreProcesssamentoDados
+
+
+class AprendizagemKNN:
+    def __init__(self, numero_neighbors) -> None:
+        __dados = PreProcesssamentoDados()
+
+        self.numero_neighbors = numero_neighbors
+
+        entradas_treino, entradas_teste, saidas_treino, saidas_teste = train_test_split(
+            __dados.entradas,
+            __dados.saidas,
+            test_size=0.3,
+            random_state=13,
+        )
+
+        modelo = KNeighborsClassifier(n_neighbors=self.numero_neighbors)
+        modelo.fit(entradas_treino, saidas_treino)
+
+        self.resultado = modelo.predict(entradas_teste)
+
+        print(
+            classification_report(
+                saidas_teste,
+                self.resultado,
+                target_names=[
+                    "Grau I - Leve",
+                    "Grau II - Moderado",
+                    "Grau III - Grave",
+                ],
+            )
+        )
